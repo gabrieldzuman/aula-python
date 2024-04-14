c25ae87a3d88ac3b3939386c6f24b4941d3b6e55
Gabriel Dzuman <83187468+gabrieldzuman@users.noreply.github.com>
2024-04-14 20:47:44 -0300

    Update fatorial.py

--- aula-python/fatorial.py
+++ aula-python/fatorial.py
@@ -1,14 +1,13 @@
-def fatorial(n):
+def calcular_fatorial(n):
     if n == 0:
         return 1
     else:
-        return n * fatorial(n-1)
+        return n * calcular_fatorial(n - 1)
 
-# Solicita o valor de "n" ao usuário
-n = int(input("Digite o valor de n: "))
+def main():
+    n = int(input("Digite o valor de n: "))
+    resultado = calcular_fatorial(n)
+    print(f"{n}! = {resultado}")
 
-# Calcula o fatorial de "n"
-resultado = fatorial(n)
-
-# Imprime o resultado
-print(f"{n}! =", resultado)
+if __name__ == "__main__":
+    main()
